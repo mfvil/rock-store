9504bb48cfd6a4e5941a8bb75345d6e622a47927
mfvil <rarefyev@gmail.com>
2022-12-15 13:04:37 -0600

    Initial commit from Create Next App

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Greeting from '@/components/Greeting'
+
+export default function IndexPage() {
+  return (
+    <div>
+      <div className="py-20">
+        <Greeting target={'Next.js Starter Kit'} />
+      </div>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  jit: false,
+  purge: ['./src/components/**/*.{js,ts,tsx}', './pages/**/*.{js,ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
